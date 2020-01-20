c3bdb61a434e3321f74d4b1d06cc916a7e05a6db
Leandro Tavares <leandro.mendestavares@gmail.com>
2020-01-19 21:10:59 -0300

    Add Categorias

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+# Django ORM - Object Relational Mapper
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+
+class Categoria(models.Model):
+    title = models.CharField(max_length=255)
