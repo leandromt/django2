84604af423dba1e67fda79f972c46f4f7baeecf7
Leandro Tavares <leandro.mendestavares@gmail.com>
2020-01-20 13:04:20 -0300

    Add Campo facultativo

--- my_app/models.py
+++ my_app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 # Django ORM - Object Relational Mapper
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    # null para o DB
+    # blank para o form
+    subtitle = models.CharField(max_length=255, null=True, blank=True)
     content = models.TextField()
 
 
