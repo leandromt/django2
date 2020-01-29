bc46f7f9b03c4dda3de86e2ad60cb755a6697024
Leandro Tavares <leandro.mendestavares@gmail.com>
2020-01-28 23:16:30 -0300

    Add page post

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 # MVC
@@ -19,3 +20,18 @@ def home(request):
 
 def home_para(request):
     return HttpResponse('Olá Mundo!')
+
+
+def post_list(request):
+    data_page = {
+        'title': 'Listagem de posts',
+        'description': 'Esta página lista todos os posts',
+        'posts': Post.objects.all()
+    }
+    return render(request, 'posts/post_list.html', data_page)
+
+
+def post_show(request, post_id):
+    post = Post.objects.get(id=post_id)
+
+    return render(request, 'posts/post_show.html', {'post': post})
